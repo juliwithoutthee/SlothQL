1a5e652fb4dc5fd15245415d71cb63b79cc02a55
juliwithoutthee <jnboucree@gmail.com>
2022-07-27 16:36:39 +0900

    Adds typeDefs variable for gql

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+ 
+  type Sloth {
+    species: String
+    
+  }
+
+
+  type Query {
+    sloths: [Sloth]
+  }
+`;
